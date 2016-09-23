7c986dea54b3f97c0e1b60aa14a3ecb3fd1b0d78
richelbilderbeek <richel@richelbilderbeek.nl>
2016-09-23 14:05:46 +0200

    Do leak memory

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
 
-int main() {
-  std::cout << "Hello world\n";
+int main()
+{
+  for (int i=0; i!=10; ++i)
+  {
+    //Allocate memory here
+    int * const p = new int;
+
+    if (!p) return 1;
+
+    //delete p; //Oop, forgotten!
+  }
 }
